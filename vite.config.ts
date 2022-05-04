aaa404be7b42b25f3a9718cdb73e86d2699faa97
Jeanluca <jeanlucafp@gmail.com>
2022-05-04 14:35:36 -0300

    export types for a lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'MyLib',
-      formats: ['es', 'umd'],
-      fileName: (format) => `my-lib.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, 'src/lib/index.ts'),
+            name: 'binno-web-widget',
+            formats: ['es', 'umd'],
+            fileName: (format) => `my-lib.${format}.js`,
         },
-      },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'tailwindcss'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'tailwindcss': 'tailwindcss',
+                },
+            },
+        },
     },
-  },
-})
+});
